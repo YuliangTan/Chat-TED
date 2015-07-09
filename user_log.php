dbaa8992a00bfca777f4367b6d40138586772804
tanyuliang <tanyuliang2@gmail.com>
2015-07-09 22:19:39 +0800

    修复成功user_log.php,让LOG保存filesystem

--- user_log.php
+++ user_log.php
@@ -1,6 +1,6 @@
 <?php
 function writeLog(){
-$logFile = 'userlogin.log';
+$logFile = '/home/vcap/fs/2ad834759b976e6/userlogin.log';
 $msg = date('Y-m-d H:i:s').' >>> '.$_GET["info"]."\r\n";
 file_put_contents($logFile,$msg,FILE_APPEND );
 }
