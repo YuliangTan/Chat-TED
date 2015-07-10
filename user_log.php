467df8b7a6b2ab7f08a5e71efaba8ecfba4cf810
tanyuliang <tanyuliang2@gmail.com>
2015-07-10 11:16:39 +0800

    使用Node.js开发，不再使用PHP

--- user_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-function writeLog(){
-$logFile = '/home/vcap/fs/2ad834759b976e6/userlogin.log';
-$msg = date('Y-m-d H:i:s').' >>> '.$_GET["info"]."\r\n";
-file_put_contents($logFile,$msg,FILE_APPEND );
-}
-writeLog();
-echo $_GET["info"];
-?>
