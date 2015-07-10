a64eb5b80353d8bd7ab7c808894d86776c395675
tanyuliang <tanyuliang2@gmail.com>
2015-07-10 15:27:31 +0800

    Add save file

--- chat.py
+++ chat.py
@@ -23,7 +23,7 @@ def env():
 def post():
     with open("/home/vcap/fs/2ad834759b976e6/login.log","a+") as fo:
         #fo.wrtie(time.asctime( time.localtime(time.time()) ))
-        fo.write(">>>>")
+        #fo.write(">>>>")
         fo.write(request.args.get('info'))
         fo.write("\n")
     return request.args.get('info')
