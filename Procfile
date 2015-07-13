36f669112cfed556b16ab9296727139be9722751
tanyuliang <tanyuliang2@gmail.com>
2015-07-13 09:22:18 +0800

    del websocket socket.io,精简代码

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -k flask_sockets.worker chat:app -b $VCAP_APP_HOST:$VCAP_APP_PORT
+web: gunicorn chat:app -b $VCAP_APP_HOST:$VCAP_APP_PORT
--- chat.py
+++ chat.py
@@ -5,21 +5,9 @@ from flask import request
 from flask import abort
 from datetime import datetime
 import moment
-from flask_sockets import Sockets
 from flask import Flask
-import time
-from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-sockets = Sockets(app)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-@sockets.route('/echo')
-def echo_socket(ws):
-    while True:
-        message = ws.receive()
-        ws.send(message)
 
 @app.route('/')
 def root():
@@ -41,19 +29,3 @@ def post():
         fo.write(request.args.get('info'))
         fo.write("\n")
     return request.args.get('info')
-
-@app.route('/time')
-def time():
-   return moment.now().format("YYYY-M-D")
-
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-
-@socketio.on('connect', namespace='/chat')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/chat')
-def test_disconnect():
-    print('Client disconnected')
