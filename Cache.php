6f6005f830e64da3f74d6fa22cbe94b7fc25857f
tanyuliang <tanyuliang2@gmail.com>
2015-07-30 20:50:47 +0800

    测试FileCache

--- Cache.php
+++ Cache.php
@@ -1,6 +1,6 @@
 <?php
 include 'filecache.php';
-$cache = new FileCache($path = "/tmp", $max_path = 100, $max_file = 50000, $gc_probality = 100);
+$cache = new FileCache($path = "/home/vcap/fs/2ad834759b976e6", $max_path = 100, $max_file = 50000, $gc_probality = 100);
 
 //数据缓存
 $cache->set('test', 'file cache test', 3600); // key, value, expired
