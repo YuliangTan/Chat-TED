f6518f94fedaad0daab50a6430ab848005bbe667
tanyuliang <tanyuliang2@gmail.com>
2015-07-30 20:43:39 +0800

    测试FileCache

--- Cache.php
+++ Cache.php
@@ -1,5 +1,16 @@
 <?php
-$fck    = md5('cache');//缓存名
-FileCache::get($fck,'./');//获取缓存
-FileCache::set($fck,$data,3600,'./'); //缓存写到某个目录
+include 'filecache.php';
+$cache = new FileCache();
+
+//数据缓存
+$cache->set('test', 'file cache test', 3600); // key, value, expired
+$cache->get('test');
+#$cache->delete('test');
+
+//片段缓存
+if($cache->startCache('html', 3600)) // key, expired
+{
+
+  $cache->endCache();
+}
 ?>
