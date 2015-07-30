ab4b27d8dea27f8cd26722fedf7ad1d95c2e362e
tanyuliang <tanyuliang2@gmail.com>
2015-07-31 07:51:55 +0800

    Fix Bug of Cache

--- check_win.php
+++ check_win.php
@@ -1,15 +1,12 @@
 <?php
 include 'filecache.php';
 $cache = new FileCache($path = "/home/vcap/fs/2ad834759b976e6", $max_path = 100, $max_file = 50000, $gc_probality = 100);
-if $_GET["del"] == 'OK'{
+if ($_GET["del"]=='OK'){
     $cache->delete($_GET["name"]);
 }
-if $_GET["check"] == 'OK'
-{
+elseif ($_GET["check"]=='OK'){
 echo $cache->get($_GET["name"]);
-}
-else
-{
+} else {
 $cache->set($_GET["name"], $_GET["txt"], 3600); // key, value, expired
 }
 ?>
