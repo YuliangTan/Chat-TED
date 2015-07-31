cc6f6e663b912ff618c05c99e927ac0054081d71
tanyuliang <tanyuliang2@gmail.com>
2015-07-31 21:55:12 +0800

    Fix Bug

--- redis-sub.php
+++ redis-sub.php
@@ -1,3 +1,4 @@
+<?php
 $redis = new Redis();
 $redis->connect('10.9.21.212',5398);
 $redis->auth('9145ef3c-9d30-43aa-b804-3aa66b79bf59');
@@ -6,3 +7,4 @@ $redis->subscribe(array('channel'.$channel), 'callback');
 function callback($instance, $channelName, $message) {
   print $channelName, "==>", $message,PHP_EOL;
 }
+?>
