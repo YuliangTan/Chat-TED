e6f5a2dd15f57ced081240ac4511b4f81ece681e
tanyuliang <tanyuliang2@gmail.com>
2015-08-01 09:16:46 +0800

    删除redis

--- redis-pub.php
+++
@@ -1,8 +0,0 @@
-<?php
-$redis = new Redis();
-$redis->connect('10.9.21.212',5398);
-$redis->auth('9145ef3c-9d30-43aa-b804-3aa66b79bf59');
-$channel = $argv[1];  // channel
-$msg = $argv[2];     // msg
-$redis->publish('channel'.$channel, $msg);
-?>
