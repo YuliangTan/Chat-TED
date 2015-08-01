7c0b0f77e799e7742a0b1cfe2e3b7b541df11ddf
tanyuliang <tanyuliang2@gmail.com>
2015-08-01 10:11:35 +0800

    Fix a Bug

--- read_cache.php
+++ read_cache.php
@@ -1,6 +1,6 @@
 <?php
 include 'filecache.php';
-$cache = new FileCache($path = "/home/vcap/fs/2ad834759b976e6", $max_path = 100, $max_file = 50000, $gc_probality = 100);
+$cache = new FileCache($path = "/home/vcap/fs/2ad834759b976e6",$max_path = 9999999, $max_file = 9999999, $gc_probality = 100);
 if ($_GET["del"]=='OK'){
     $cache->delete($_GET["name"]);
 }
