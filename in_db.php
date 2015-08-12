b65bdd8f1fecb1f1b8f5e1cec5105078052246d7
tanyuliang <tanyuliang2@gmail.com>
2015-08-12 03:28:54 +0000

    Sqlite Test2

---
+++ in_db.php
@@ -0,0 +1,28 @@
+<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('/home/vcap/fs/2ad834759b976e6/tylchat.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      INSERT INTO USER (NAME,PASS)
+      VALUES ('Paul', 'California');
+EOF;
+
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Records created successfully\n";
+   }
+   $db->close();
+?>
