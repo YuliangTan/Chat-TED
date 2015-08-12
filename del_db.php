544c580d2db94cfe0a8ad6e3f694d88fa84ebced
tanyuliang <tanyuliang2@gmail.com>
2015-08-12 03:40:45 +0000

    Sqlite Test3

---
+++ del_db.php
@@ -0,0 +1,34 @@
+<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('/home/vcap/fs/2ad834759b976e6/tylchat.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+   $sql =<<<EOF
+      DELETE from USER where NAME='Paul';
+EOF;
+   $ret = $db->exec($sql);
+   if(!$ret){
+     echo $db->lastErrorMsg();
+   } else {
+      echo $db->changes(), " Record deleted successfully\n";
+   }
+
+   $sql =<<<EOF
+      SELECT * from USER;
+EOF;
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      echo "NAME = ". $row['NAME'] ."\n";
+   }
+   echo "Operation done successfully\n";
+   $db->close();
+?>
