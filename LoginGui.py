2c6375bb432f146c899092919f1bbffd18e3f615
tanyuliang <tanyuliang2@Gmail.com>
2015-08-16 17:28:21 +0800

    添加注册功能尚未完工

--- LoginGui.py
+++ LoginGui.py
@@ -29,17 +29,27 @@ class prpcrypt():
         cryptor = AES.new(self.key, self.mode, self.key)
         plain_text = cryptor.decrypt(a2b_hex(text))
         return plain_text.rstrip('\0')
+    def encrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.key)
+        length = 32
+        count = len(text)
+        add = length - (count % length)
+        text = text + ('\0' * add)
+        self.ciphertext = cryptor.encrypt(text)
+        return b2a_hex(self.ciphertext)
+
 class LoginFrame(wx.Frame):
     def __init__(self, parent, id, title, size):
         wx.Frame.__init__(self, parent, id, title)
         self.SetSize(size)
         self.Center()
-        self.passWordLabel = wx.StaticText(self, label = _("UserName"), pos = (30, 50), size = (120, 25))
-        self.userNameLabel = wx.StaticText(self, label = _("Password"), pos = (30, 100), size = (120, 25))
-        self.userName = wx.TextCtrl(self, pos = (100, 47), size = (150, 25),style=wx.TE_PROCESS_ENTER)
-        self.passWord= wx.TextCtrl(self, pos = (100, 97), size = (150, 25),style=wx.TE_PASSWORD|wx.TE_PROCESS_ENTER)
+        self.passWordLabel = wx.StaticText(self, label = _("UserName"), pos = (30, 40), size = (120, 25))
+        self.userNameLabel = wx.StaticText(self, label = _("Password"), pos = (30, 90), size = (120, 25))
+        self.userName = wx.TextCtrl(self, pos = (85, 37), size = (150, 25),style=wx.TE_PROCESS_ENTER)
+        self.passWord= wx.TextCtrl(self, pos = (85, 87), size = (150, 25),style=wx.TE_PASSWORD|wx.TE_PROCESS_ENTER)
         self.userName.Bind(wx.EVT_TEXT_ENTER,self.login_thread)
         self.passWord.Bind(wx.EVT_TEXT_ENTER,self.login_thread)
+        self.cb = wx.CheckBox(self, pos=(30,120) ,label="I am new user", size=(120,25))
         self.loginButton = wx.Button(self, label = _('Login'), pos = (80, 145), size = (130, 30))
         self.loginButton.Bind(wx.EVT_BUTTON,self.login_thread)
         pub.subscribe(self.__Friend_list, 'list.show')
@@ -55,38 +65,24 @@ class LoginFrame(wx.Frame):
             thread.start_new_thread(self.login, ())
             self.loginButton.Disable()
     def login(self):
-            if not self.userName.GetValue():
+         if not self.userName.GetValue():
                    wx.CallAfter(wx.MessageBox,_('Please enter the username'), _('Error'), 
                    wx.OK | wx.ICON_ERROR)
                    wx.CallAfter(self.loginButton.Enable)
-            elif not self.passWord.GetValue():
+         elif not self.passWord.GetValue():
                     wx.CallAfter(wx.MessageBox,_('Please enter the password'), _('Error'), 
                     wx.OK | wx.ICON_ERROR)     
                     wx.CallAfter(self.loginButton.Enable)      
-            else:
+         else:
+           if self.cb.IsChecked()==False:
               try:
                 passwd = urllib2.urlopen("http://chat-tyl.coding.io/put_db.php?content=PASS&db=USER&where=NAME&where_t=" + self.userName.GetValue()).read()
               except urllib2.HTTPError,e:
                 wx.CallAfter(wx.MessageBox,_('Unable to fetch data'),_('Error'), wx.OK | wx.ICON_ERROR)
                 wx.CallAfter(self.loginButton.Enable)                                   
 	      passwd0 = pc.decrypt(passwd)
-              if self.passWord.GetValue()==passwd0:
-                 #try:
-                        #cursor.execute("SELECT uncompress(Data) FROM friendlist WHERE name = '%s' LIMIT 1"%(self.userName.GetValue()))
-                        #data = json.loads(cursor.fetchone()[0])
-                        #cursor.close()
-                        #conn.close()
-                        #db.close()
-                    #except IOError, e:      
-                          #wx.CallAfter(wx.MessageBox,'Error %d: %s' % (e.args[0], e.args[1]),_('Try it again'), 
-                          #wx.OK | wx.ICON_ERROR) 
-                          #wx.CallAfter(self.loginButton.Enable)     
-                    #urllib2.urlopen('http://chat-tyl.coding.io/user_log?info=User___'+self.userName.GetValue()+'___Login')
-                    #time.sleep(0.1)
-                    #wx.CallAfter(Notify.init,"Chat-TYL")
-                    #bubble_notify = Notify.Notification.new (_("Information"),_("Login Successful"),"file://" + os.path.abspath(os.path.curdir) + "/Chat-TYL.ico")
-                    #wx.CallAfter(bubble_notify.show) 
-                    #wx.CallAfter(pub.sendMessage,'list.show', data)
+              if self.passWord.GetValue()==passwd0: 
+                 #urllib2.urlopen('http://chat-tyl.coding.io/user_log?info=User___'+self.userName.GetValue()+'___Login')
                  try:
                    data = urllib2.urlopen("http://chat-tyl.coding.io/put_db.php?content=LIST&db=FRIEND&where=NAME&where_t=" + self.userName.GetValue()).read()
                  except urllib2.HTTPError,e:
@@ -97,7 +93,29 @@ class LoginFrame(wx.Frame):
               else:
                 wx.CallAfter(wx.MessageBox,_('Your Password is wrong'), _('Try it again'), 
                 wx.OK | wx.ICON_ERROR)
-                wx.CallAfter(self.loginButton.Enable)    
+                wx.CallAfter(self.loginButton.Enable) 
+           else:
+             try:
+               cont = urllib2.urlopen("http://chat-tyl.coding.io/put_db.php?content=PASS&db=USER&where=NAME&where_t=" + self.userName.GetValue()).read()
+             except urllib2.HTTPError,e:
+               wx.CallAfter(wx.MessageBox,_('Check Your NetWork,and try it again'),_('Error'), wx.OK | wx.ICON_ERROR)
+               wx.CallAfter(self.loginButton.Enable)
+             if not cont:   
+               try:
+                passwd = urllib2.urlopen("http://chat-tyl.coding.io/in_db.php?db=USER&first=NAME&seconed=PASS&name=" + self.userName.GetValue() + "&pass=" + pc.encrypt(self.passWord.GetValue())).read()
+               except urllib2.HTTPError,e:
+                 wx.CallAfter(wx.MessageBox,_("We can't register,check your network and try it again"),_('Error'), wx.OK | wx.ICON_ERROR)
+                 wx.CallAfter(self.loginButton.Enable)    
+               try:
+                list = urllib2.urlopen("http://chat-tyl.coding.io/in_db.php?db=FRIEND&first=NAME&seconed=LIST&name=" + self.userName.GetValue() + "&pass={'item': ['friend'],'friend' : ['tyl','Test']}").read()
+               except urllib2.HTTPError,e:
+                 wx.CallAfter(wx.MessageBox,_("We can't register,check your network and try it again"),_('Error'), wx.OK | wx.ICON_ERROR)
+                 wx.CallAfter(self.loginButton.Enable) 
+               wx.CallAfter(wx.MessageBox,_("Register Successful"),_('Error'), wx.OK | wx.ICON_ERROR)
+               wx.CallAfter(self.loginButton.Enable)
+             else:
+               wx.CallAfter(wx.MessageBox,_('You already registered'),_('Error'), wx.OK | wx.ICON_INFORMATION)
+               wx.CallAfter(self.loginButton.Enable)       
 if __name__ == '__main__':
     pc = prpcrypt('keyskeyskeyskeys')
     app = wx.App()
