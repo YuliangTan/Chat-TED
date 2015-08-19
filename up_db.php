181d7a0532b1504f276d762c3348d095106db67c
tanyuliang <tanyuliang2@Gmail.com>
2015-08-19 15:08:28 +0800

    add up_db.php

---
+++ up_db.php
@@ -0,0 +1,30 @@
+<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('test.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+   $sql ="UPDATE USER set " . $_GET['label'] . "=" . "'" . 
+$_GET['content'] . "'" . 
+"where " . 
+$_GET['name'] 
+. "=" . "'" . $GET['ch'] . "'" 
+. 
+";";
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo $db->changes(), " Record updated successfully\n";
+   }
+   echo "Operation done successfully\n";
+   $db->close();
+?>
